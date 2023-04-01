6c878148e2823a4919907308511e48aa879c2d89
Joffrey Trebot <trebotjoffrey@gmail.com>
2023-04-01 21:31:46 +0200

    feat(vite): update configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv, } from "vite";
+import { configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    setupFiles: "src/setupTests.ts",
-  },
-  root: ".",
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      setupFiles: "src/setupTests.ts",
+    },
+    root: ".",
+  };
+});
