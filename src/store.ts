2dfa5b5cf305e2f214586821869f3a2c82f768a1
Joffrey Trebot <trebotjoffrey@gmail.com>
2023-04-01 21:32:52 +0200

    feat(store): configure redux store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ReportReducer from "./slices/ReportSlice";
 
-export const store = configureStore({ reducer: { reports: ReportReducer } });
+export const store = configureStore({
+  reducer: { report: ReportReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
